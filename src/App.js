509b31117fe86226e489e56514b7269353a7bd31
Kaylea <kayleapaup@yahoo.com>
2024-08-10 12:56:28 -0500

    Created search engine and function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
-import logo from "./logo.png"
+import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-<img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
-    </div>
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            Coded by Kaylea Paup
+            </small>
+        </footer>
+      </div>
+      </div>
   );
 }
