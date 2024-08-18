53ad05881231c70f105c1edae72feb03db12979a
Kaylea <kayleapaup@yahoo.com>
2024-08-17 20:09:47 -0500

    Fixed phonetics function

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -3,9 +3,6 @@ import React from "react";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <a href={props.phonetic} target="_blank" rel="noopener noreferrer">
-        Listen
-      </a>
       <span className="text">{props.phonetic}</span>
     </div>
   );
--- src/Results.js
+++ src/Results.js
@@ -7,7 +7,7 @@ export default function Results(props) {
     return <div className="Results">
         <section>
         <h2>{props.results.word}</h2>
-        <Phonetics props={props.results.phonetics} />
+        <Phonetics phonetic={props.results.phonetic} />
         </section>
           {props.results.meanings.map(function (meaning, index){
               return (
